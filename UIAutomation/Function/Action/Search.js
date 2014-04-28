954309c849c205dc26f0e391c61ac65cd14d2395
zhidongzhang <zhidong.zhang@symbio.com>
2014-04-28 18:09:36 +0800

    update search

--- UIAutomation/Function/Action/Search.js
+++ UIAutomation/Function/Action/Search.js
@@ -1,15 +1,16 @@
 Action.tapSearchIconOnNavBar = function () {
     $.delay(sleep);
-    app.navigationBar().buttons()[2].tap();
+    var tapSearchIconOnNavBar = app.navigationBar().buttons()[2];
+    tapSearchIconOnNavBar.tap();
     app.mainWindow();
 };
 
 Action.tapBackOnSearchBar = function () {
     $.delay(sleep);
-    app.mainWindow().buttons()[1].tap();
+    var tapBackOnSearchBar = app.mainWindow().buttons()[1];
+    tapBackOnSearchBar.tap();
 };
 
-//Click search, and input
 Action.searchBarInput = function (skey) {
     $.delay(sleep);
     var app = target.frontMostApp();
@@ -18,102 +19,143 @@ Action.searchBarInput = function (skey) {
 
 Action.tapIconPlusOnTableView = function () {
     $.delay(sleep);
-     app.mainWindow().tableViews()[1].cells()[0].buttons()[0].tap();
+    var tapIconPlusOnTableView = app.mainWindow().tableViews()[1].cells()[1].buttons()[0];
+    tapIconPlusOnTableView.tap();
 };
 
 //Click the icon plue different twice
 Action.clickOnTheDifferentIconPlus = function () {
     $.delay(sleep);
-     app.mainWindow().tableViews()[1].cells()[0].buttons()[0].tap();
-    $.delay(sleep);
-     app.mainWindow().tableViews()[1].cells()[0].buttons()[0].tap();
-};
-//----------------------------------------4.17
-Action.tapKeyboardSearch = function () {
-    $.delay(sleep);
-    var app = target.frontMostApp();
-    app.keyboard().buttons()["Search"].tap();
+    var clickOnTheDifferentIconPlus = app.mainWindow().tableViews()[1].cells()[0].buttons()[0];
+    clickOnTheDifferentIconPlus.tap();
 };
 
-//---------------------------------------4.18
+Action.tapKeyboardSearch = function () {
+    $.delay(sleep);
+    var tapKeyboardSearch = app.keyboard().buttons()["搜尋"];
+    tapKeyboardSearch.tap();
+};
+
 Action.tapClean = function () {
-     app.mainWindow().textFields()[0].buttons()[0].tap();
+    $.delay(sleep);
+    var tapClean = app.mainWindow().textFields()[0].buttons()[0];
+    tapClean.tap();
 };
 
 Action.tapKeyboardDelete = function () {
     $.delay(sleep);
     var app = target.frontMostApp();
-    app.keyboard().keys()["Delete"].tap();
+    var tapKeyboardDelete = app.keyboard().keys()["Delete"];
+    tapKeyboardDelete.tap();
 };
+
 
 //clean Searches
 Action.cleanSearches = function () {
     $.delay(sleep);
-    //Tap setting menu button.
-    var settingButton = app.navigationBar().buttons()[1];
-    settingButton.tap();
-    $.delay(sleep);
-
-    //Select setting on Tools
-    var settingsButton = app.windows()[0].tableViews()[0].cells()[9];
-    settingsButton.tap();
+    var menuButton = app.navigationBar().buttons()[1];
+    menuButton.tap();
     $.delay(sleep);
     
-    app.windows()[0].collectionViews()[0].cells()[1].logElementTree();
-    //tap clean search history button.   
-    var cleanSearchHistoryButton = app.windows()[0].collectionViews()[0].cells()["搜尋記錄"];
-    cleanSearchHistoryButton.tap();
-
-    //Tap OK button on 確定 alert window.
+    var tableViews = app.windows()[0].tableViews()[0];
+    var tableCells = tableViews.cells()[3];
+    tableCells.tap();
+    $.delay(sleep);
+    
     UIATarget.onAlert = function onAlert() {
         return true;
-    };
-
-    app.alert().defaultButton().tap();
+    }
+    var collectionViews = app.windows()[0].collectionViews()[0];
+    var cleanSearchHistory = collectionViews.cells()[1].staticTexts()[0];
+    cleanSearchHistory.tap();
     $.delay(sleep);
-
-    //Tap close button.
-    app.navigationBar().buttons()[1].tap();
-
-    //Tap menu button to navigate to discovery screen.
-    settingButton.tap();
+    
+    var determineButton = app.alert().buttons()[1];
+    determineButton.tap();
+    
+    var closeButton = app.windows()[0].navigationBar().buttons()[1];
+    closeButton.tap();
+    menuButton.tap();
 };
 
-
-//-----------------4.20
-Action.searchBarInputChinese = function () {
+Action.searchBarInputChinese = function (sValue) {
     $.delay(sleep);
     var app = target.frontMostApp();
-     app.mainWindow().textFields()[0].setValue("東");
+    var searchField = app.mainWindow().textFields()[0];
+    searchField.setValue(sValue);
 };
 
-//Action.tapReturnOnSearchBar = function () {
-//    $.delay(sleep);
-//     app.mainWindow().buttons()[1].tap();
-//};
-
-//Action.doubleClickReturnButton = function () {
-//    $.delay(sleep);
-//    app.mainWindow().navigationBar().buttons()[0].tap();
-//    $.delay(5);
-//     app.mainWindow().buttons()[0].tap();
-//};
+Action.tapReturnOnSearchBar = function () {
+    $.delay(sleep);
+    app.mainWindow().buttons()[0].tap();
+};
 
 Action.tapIconPlusOnFirstFloorTableView = function () {
     $.delay(sleep);
-     app.mainWindow().tableViews()[0].cells()[0].buttons()[0].tap();
-};
+    var tapIconPlusOnFirstFloorTableView = app.mainWindow().tableViews()[0].cells()[1].buttons()[0];
+    tapIconPlusOnFirstFloorTableView.tap();
+ };
 
 //Click the icon plue different twice
 Action.clickOnTheDifferentIconPlusOnFirstFloorTableView = function () {
     $.delay(sleep);
-    app.mainWindow().tableViews()[0].cells()[0].buttons()[0].tap();
-    $.delay(sleep);
-    app.mainWindow().tableViews()[0].cells()[0].buttons()[0].tap();
+    var  clickOnTheDifferentIconPlusOnFirstFloorTableView = app.mainWindow().tableViews()[0].cells()[0].buttons()[0];
+    clickOnTheDifferentIconPlusOnFirstFloorTableView.tap();
 };
 
 Action.goBackOnSearchPage = function () {
     $.delay(sleep);
     var goBack = app.navigationBar().buttons()[2];
     goBack.tap();
+};
+
+//04-23-2014
+Action.goApparelCategory = function (){
+    $.delay(sleep);
+    app.tabBar().buttons()[2].tap();
+    
+    $.delay(sleep);
+    app.mainWindow().tableViews()[0].cells()[0].tap();
+};
+
+Action.backToAllCategory = function () {
+    $.delay(sleep);
+    var backToAllCategory = app.navigationBar().buttons()[1];
+    backToAllCategory.tap();
+};
+
+Action.goDiscoveryStream = function () {
+    $.delay(sleep);
+    app.tabBar().buttons()[0].tap();
+};
+
+Action.tapSearchIconOnApparelCategory = function () {
+    var tapSearchIconOnApparelCategory = app.navigationBar().buttons()[2];
+    tapSearchIconOnApparelCategory.tap();
+};
+
+//04-25-2014
+Action.goFashionWomenClothingCategory = function () {
+    $.delay(sleep);
+    var goFashionWomenClothingCategory = app.mainWindow().collectionViews()[0].cells()[3];
+    goFashionWomenClothingCategory.tap();
+};
+
+Action.tapBackSubclassification = function () {
+    $.delay(sleep);
+    var tapBackSubclassification = app.navigationBar().buttons()[1];
+    tapBackSubclassification.tap();
+};
+
+Action.goCoatCategory = function () {
+    $.delay(sleep);
+    var goCoatCategory = app.mainWindow().collectionViews()[0].cells()[1];
+    goCoatCategory.tap();
+};
+
+//04-28-2014
+Action.tapGoodsButton = function () {
+    $.delay(sleep);
+    var tapGoodsButton = app.mainWindow().collectionViews()[0].cells()[0].buttons()[1];
+    tapGoodsButton.tap();
 };
--- UIAutomation/Function/Assert/Search.js
+++ UIAutomation/Function/Assert/Search.js
@@ -14,14 +14,15 @@ Assert.backButtonOnSearchBar = function () {
     $.delay(sleep);
     assertEquals("icon back", app.mainWindow().buttons()[1].name());
 };
-//----------------------------------------4.16
-Assert.checkReturnPageDisplay =function () {
+
+Assert.checkReturnPageDisplay =function (text) {
     $.delay(sleep);
-    assertEquals("最新動態", app.mainWindow().navigationBar().name());
+    assertEquals(text, app.mainWindow().navigationBar().name());
 };
 
 //Check if search Suggestions
 Assert.autoCompletePageDisplay = function () {
+    $.delay(sleep);
     var mainWindow = app.mainWindow();
     $.delay(5);
     assertTrue(app.mainWindow().tableViews()[1].cells().length<20);
@@ -33,16 +34,9 @@ Assert.checkIconPlusDisplay = function () {
     assertEquals("icon plus", app.mainWindow().tableViews()[1].cells()[0].buttons()[0].name());
   };
 
-//----------------------------------------4.17
-
-Assert.searchResultsPage = function () {
-    $.delay(sleep);
-    assertEquals("\"hl中大尺碼\"", app.navigationBar().name());
-   
-};
-//----------------------------------------4.18
 //check whether there is a key
 Assert.checkCleanIconDisplay = function () {
+    $.delay(sleep);
     var mainWindow = app.mainWindow();
     var cleanIcon = app.mainWindow().textFields()[0].buttons()[0].isEnabled();
     if (cleanIcon != 1){
@@ -54,15 +48,18 @@ Assert.checkCleanIconDisplay = function () {
 };
 
 Assert.checkCleanIcon = function () {
-    assertEquals("Clear text",app.mainWindow().textFields()[0].buttons()[0].name());
+    $.delay(sleep);
+    assertEquals("清除文字",app.mainWindow().textFields()[0].buttons()[0].name());
 };
 
 //Verify whether the clear one character at a time
 Assert.checkEveryDelete = function () {
+    $.delay(sleep);
     assertEquals("123",app.mainWindow().textFields()[0].value());
 };
 
 Assert.checkSearchPage = function (skey) {
+    $.delay(sleep);
     assertEquals(skey, app.mainWindow().navigationBar().name());
 };
 
@@ -70,4 +67,65 @@ Assert.searchSuggestionsPageDisplay = function () {
     var mainWindow = app.mainWindow();
     $.delay(5);
     assertTrue(app.mainWindow().tableViews()[1].cells().length<1);
+};
+
+//04-23-2014
+Assert.searchButtonOnApparelCategoryIsEnabled = function () {
+    $.delay(sleep);
+    
+    //Search button position
+    var searchButtonValue = app.navigationBar().buttons()[2].isEnabled();
+    assertEquals(1, searchButtonValue);
+};
+
+//04-24-2014
+Assert.tapIconPlusOnTableViewCheckTextIsEnabled = function (sKey) {
+    $.delay(sleep);
+    //To obtain search box text and check search box text show the correct
+    var textValue = app.mainWindow().textFields()[0].value();
+    assertEquals(sKey,textValue);
+};
+
+Assert.clickOnTheDifferentIconPlusCheckTextIsEnabled = function (sKey) {
+    $.delay(sleep);
+    //To obtain search box text and check search box text show the correct
+    var textValue = app.mainWindow().textFields()[0].value();
+    assertEquals(sKey,textValue);
+};
+
+Assert.clickOnTheDifferentIconPlusOnFirstFloorTableView = function (dKey) {
+    $.delay(sleep);
+    //To obtain search box text and check search box text show the correct
+    var textValue = app.mainWindow().textFields()[0].value();
+    assertEquals(dKey,textValue);
+};
+
+Assert.searchResultsPage = function (sKey) {
+    $.delay(sleep);
+    //Verify the search page display properly
+    var searchResultsPageName = app.navigationBar().name();
+    assertEquals('"'+sKey+'"',searchResultsPageName);
+};
+
+//04-25-2014
+Assert.longtextSearchPageDisplay = function () {
+   $.delay(sleep);
+   assertTrue(app.mainWindow().collectionViews()[0].cells().length<=1);
+};
+
+//04-28-2014
+Assert.coatCategorySearchResultsPageDisplayedInList = function () {
+    $.delay(sleep);
+    var coatCategorySearchResultsPageDisplayedInList = app.mainWindow().navigationBar().staticTexts()[1].name();
+    assertEquals("搜尋上衣",coatCategorySearchResultsPageDisplayedInList);
+};
+
+Assert.checkTypeIphoneShowTheCorrect = function () {
+    this.checkTypeIphoneShowTheCorrect = [
+                               "【Apple I phone 5 專用】5S 適用 U case 鋼化玻璃螢幕保護貼 手機 螢幕保貼 保貼 抗指紋/高清/耐刮",
+                               "Apple I phone 4S/iPhone 4 超薄磨砂硬式保護殼 英字簽名 金屬/磨砂 保護殼 手機殼 手機套 背殼 硬殼"
+                               ];
+    $.delay(10);
+    assertEquals(this.checkTypeIphoneShowTheCorrect[0], app.mainWindow().collectionViews()[0].cells()[1].staticTexts()[2].name());
+    assertEquals(this.checkTypeIphoneShowTheCorrect[1], app.mainWindow().collectionViews()[0].cells()[2].staticTexts()[2].name());
 };
